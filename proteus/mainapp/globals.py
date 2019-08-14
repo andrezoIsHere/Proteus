519bc35a1cdadf54662bf9df7c45e55096560211
Andrey Lyapeikov <andreandnike@gmail.com>
2019-08-14 16:19:23 +0600

    Working with frontend

--- proteus/mainapp/globals.py
+++ proteus/mainapp/globals.py
@@ -1,7 +1,16 @@
 site = {
+
     'global': {
         'description': 'Динамическая новостная лента для ваших предпочтений',
         'name': 'Proteus',
-        'title': 'Proteus - новостная лента для вашего использования'
-        }
+        'charset': 'utf-8',
+        'keywords': '''Новости, новостная лента, Proteus, andrezo, Новости Казахстан, Казахстанские новости''',
+        'title': 'Proteus - новостная лента для вашего использования',
+        'author': 'andrezoIsHere'
+        },
+
+    'settings': {
+        'viewport': 'width=device-width, initial-scale=1, maximum-scale=1'
+    }
+
 }
--- proteus/mainapp/urls.py
+++ proteus/mainapp/urls.py
@@ -18,6 +18,10 @@ Including another URLconf
 from django.conf.urls import include, url
 from . import views
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 urlpatterns = [
     url('', views.index, name='index')
 ]
+
+urlpatterns += staticfiles_urlpatterns()
